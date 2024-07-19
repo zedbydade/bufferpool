ef5b81434ddae4259d846dfd8350d728dc0f7696
Francisco Paradela <franciscoleite.dev@protonmail.com>
2024-07-18 23:19:47 -0300

    Add some buffer allocation

--- src/main.c
+++ src/main.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-  printf("Tu-Tu-Ru");
+  int* buffer;
+  int allocate_indexes = 0;
+
+  buffer = malloc(sizeof(int) * 10);
+
+  if (!buffer) {
+    printf("Failed to alocate buffer");
+  } else {
+    while (1) { 
+      int n;
+      int free_memory = sizeof(*buffer) - allocate_indexes;
+      printf("The array free space is %d", free_memory);
+      printf("Number of indexes: ");
+      scanf("%d", &n);
+      allocate_indexes += n;
+    }
+  }
 }
