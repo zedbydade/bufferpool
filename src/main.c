3498404336d0d7af9ae810a82f25d86937c37e6a
Francisco Paradela <franciscoleite.dev@protonmail.com>
2024-07-26 22:43:24 -0300

    Add little guard

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main() {
-  int* buffer;
+  int *buffer;
   int allocate_indexes = 0;
 
   buffer = malloc(sizeof(int) * 10);
@@ -10,12 +10,16 @@ int main() {
   if (!buffer) {
     printf("Failed to alocate buffer");
   } else {
-    while (1) { 
+    while (1) {
       int n;
       int free_memory = sizeof(*buffer) - allocate_indexes;
-      printf("The array free space is %d", free_memory);
+      printf("The array free space is %d \n", free_memory);
       printf("Number of indexes: ");
       scanf("%d", &n);
+      if (n > free_memory) {
+        printf("It is nos possible to allocate that amount of memory \n");
+        continue;
+      }
       allocate_indexes += n;
     }
   }
